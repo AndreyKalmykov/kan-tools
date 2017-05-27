e5d0ed917d746158d9168699e957730825ba3dd9
Andrey Kalmykov <kalmykov.a.n@gmail.com>
2017-05-27 10:37:40 +0300

    текучка

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@
 #include "ktMainForm.h"
 
 int main(int argc, char **argv) {
-  std::string imName= (std::string)dirname(argv[0])+(std::string)"/kTools.png";
-  //~ imName += "/kTools.png";
+  std::string icFileName= (std::string)dirname(argv[0])+(std::string)"/kTools.png";
+  //~ icFileName += "/kTools.png";
 
-  //~ std::cout << imName << std::endl;
+  std::cout << icFileName << std::endl;
 
   ktMainForm mf{800,450}; //= new ktMainForm();
-  Fl_PNG_Image appIcon{imName.c_str()};
+  Fl_PNG_Image appIcon{icFileName.c_str()};
   mf.icon(&appIcon);
 
   mf.show(argc, argv);
