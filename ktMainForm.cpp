c3b9d4bf617e14224b70c5b6a803e1568d00fed2
Andrey Kalmykov <kalmykov.a.n@gmail.com>
2017-05-27 18:46:19 +0300

    исправление ошибки с программным перемещением разделителя

--- ktMainForm.cpp
+++ ktMainForm.cpp
@@ -3,13 +3,11 @@
 #include "ktMainForm.h"
 
 ktMainForm::ktMainForm(int sW,int sH):Fl_Window(sW,sH,"main form"){
-  hotspot(0,0,0);
-  //~ resizable(this);
-  resizable(tl);
   label("kan Tools");
+  hotspot(0,0,0);
+  resizable(this);
 
-  //~ icon("kan.png");
-
+  tl.resizable(r);
   tl.align(FL_ALIGN_CLIP|FL_ALIGN_INSIDE|FL_ALIGN_WRAP);
 
   begin();
@@ -21,9 +19,16 @@ ktMainForm::ktMainForm(int sW,int sH):Fl_Window(sW,sH,"main form"){
 ktMainForm::~ktMainForm(){};
 
 void ktMainForm::menuShow(){
+  std::cout << "resizable= " << tl.resizable()->label() << " childrens= " << tl.children() << std::endl;
+  tl.init_sizes();
   if (pnBar.get_btnMenuState()) {
-    tl.position(1,0,(int)(tl.w()*0.3),0);
+    tl.position(dx,0,(int)(tl.w()*0.3),0);
+  //~ std::cout << "resizable = " << tl.resizable()->label() << std::endl;
   } else {
-    tl.position(pnMenu.w(),0,1,0);
+  std::cout << "pnMenu = " << pnMenu.x() << "*" << pnMenu.w() << std::endl;
+  std::cout << "pnWrk = " << pnWrk.x() << "*" << pnWrk.w() << std::endl;
+    tl.position(pnMenu.w(),0,dx,0);
+  std::cout << "pnMenu = " << pnMenu.x() << "*" << pnMenu.w() << std::endl;
+  std::cout << "pnWrk = " << pnWrk.x() << "*" << pnWrk.w() << std::endl;
   }
 };
--- ktMainForm.h
+++ ktMainForm.h
@@ -11,8 +11,10 @@
 
 class ktMainForm: public Fl_Window {
   ktPnBar pnBar{0,0,w(),30};
+  int dx = 20, dy = dx; // border width of resizable() - see below
 
   Fl_Tile tl{x(),y()+pnBar.h(),w(),h()-pnBar.h(),"tl"};
+  Fl_Box r{tl.x()+dx,tl.y()+dy,tl.w()-2*dx,tl.h()-2*dy,"rBox"};
 
   ktPnMenu pnMenu{tl.x(),tl.y(),(int)(tl.w()*0.3),tl.h()};
   ktPnWrk pnWrk{tl.x()+pnMenu.w(),tl.y(),tl.w()-pnMenu.w(),tl.h()};
